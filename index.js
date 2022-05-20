bfb89493990cd9d12e56f663d5c0c1d0bfa0655a
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2022-05-20 17:45:51 -0600

    updated api call gmail

--- index.js
+++ index.js
@@ -1,100 +1,94 @@
+// Key for Gmail: AIzaSyAiZZkTsNqFBWPJ4o2_jyo2mQF6p35uxCI
+
+//AIzaSyCMkgmvMbk5O6KEEZM9lj1SeEDXoKlGGZY
+
 const fs = require('fs');
-const http = require('http');
 const readline = require('readline');
-const gmail = require(`@googleapis/gmail`);
+const {google} = require('googleapis');
 
-// const auth = new gmail.auth.GoogleAuth({
-//     keyFilename: '',
-//     scopes: ['']
-// });
-
-
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',"https://www.googleapis.com/auth/pubsub"];
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
 const TOKEN_PATH = 'token.json';
 
-fs.readFile('creditials.json',(err, content) =>{
-    if(err) return console.log('message goes here', err);
-    authorize(JSON.parse(content), (auth) =>{
-        listUnreadMsgs(auth), watchMyLabel(auth)
-    });
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Gmail API.
+  authorize(JSON.parse(content), listLabels);
 });
 
-// @param {Object}
-// @param {function}
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
 
-function authorize(creditials, callback){
-    const {
-        client_secret,
-        client_id,
-        redirect_uris
-    } = creditials.installed;
-    const oAuth2Client = new google.auth.oAuth2(
-        client_id, client_secret, redirect_uris[0]
-    );
-
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-    });
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
 }
 
-function getNewToken(oAuth2Client, callback){
-    const authURL = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
     });
-    rl.question('message here:', (code) =>{
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) =>{
-            if(err) return console.error('error', err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) =>{
-                if(err) return console.error(err);
-                console.log('token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+  });
 }
 
-async function listUnreadMsgs(auth){
-    var gmail = google.gmail({
-        auth: auth,
-        version: 'v1'
-    });
-
-    gmail.users.history.list({
-        userId: 'me',
-        startHistoryId: 2982217,
-        labelId: 'Label_8061975816208384485'
-    }, async function (err, results){
-        if(err) return console.log(err);
-        const latest = await results.data.history[results.data.history.length -1].messages;
-        gmail.users.messages.get({
-            userId:'me',
-            id: latest[latest.length -1].id
-        }, (err, res) => {
-            if (res.data.labelIds.includes('UNREAD')){
-                console.log(res.data.snippet);
-            }else{
-                console.log("no unread Messages here!");
-            }
-        });
-    });
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.labels.list({
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = res.data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
 }
-
-async function watchMyLabel(auth){
-    const gmail = google.gmail({
-        version: 'v1',
-        auth
-    });
-    const res = await gmail.users.watch({
-        userId: 'me',
-        requestBody: {
-            labelIds: ['Label_8061975816208384485', 'UNREAD'],
-            labelFilterAction: 'include',
-            topicName: 'projects/quickstart-1593237046786/topics/notifications'
-        }
-    })
-}
-// Key for Gmail: AIzaSyAiZZkTsNqFBWPJ4o2_jyo2mQF6p35uxCI
