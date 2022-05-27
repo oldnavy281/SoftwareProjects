0185ea9f47ea78b6536720935f3dbbb4a7ece8f6
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-05-27 10:59:48 -0600

    try to see if exports.whatever will work

--- Discord/discord.js
+++ Discord/discord.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () =>{
-    console.log(`logged in as ${client.user.tag}`);
-});
-
-client.on("message", msg =>{
-    if(msg.content === 'helo'){
-        msg.reply('eeeee');
-    }
-})
-
-client.login(process.env.OTc5MTg1OTU3MDc1MDMwMTQ2.Gc7BUm.ALr92zcm2uLX7xFm3aGJ23G_aJZXgHMSlBffjo);
+exports.DIS = () =>{
+    client.on('ready', () =>{
+        console.log(`logged in as ${client.user.tag}`);
+    });
+    
+    client.on("message", msg =>{
+        if(msg.content === 'helo'){
+            msg.reply('eeeee');
+        }
+    })
+    
+    client.login(process.env.OTc5MTg1OTU3MDc1MDMwMTQ2.Gc7BUm.ALr92zcm2uLX7xFm3aGJ23G_aJZXgHMSlBffjo);
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pug = require('pug');
 const routes = require('./routes/routes');
 const path = require('path');
+// const discord = require('./Discord/discord');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,6 @@ app.set('view engine', 'pug');
 app.set('views', __dirname+ '/views');
 
 app.get('/', routes.index);
-app.get('/gmail', routes.index)
+app.get('/gmail', routes.index);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
